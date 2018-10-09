7bfb4e1daba925599fc9177e43f1eb10f7e17476
dana.yohann@gmail.com <dana.yohann@gmail.com>
2018-10-09 09:18:20 +0100

    Finition de l'ajout du commentaire sans le pseudo

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
     try{
         if(isset($_GET['action']))
         {
-            if(isset($_GET['id']))
+            if(isset($_GET['id']) AND $_GET['action'] == "postView")
             {
                 if($_GET['id'] > 0)
                     post();
             }
             elseif ($_GET['action'] == "addComment") 
             {
-                addComment($_POST['id_post'], $_SESSION['pseudo'], $_POST['message']);
+                addComment($_POST['id_post'], "Except Session pseudo", $_POST['message']);
             }
         }
         else
--- view/frontend/postView.php
+++ view/frontend/postView.php
@@ -1,6 +1,6 @@
  <?php $title = htmlspecialchars($post['titre']) ?>
 <?php ob_start();?>
-        <a href = "index.php">
+        <a href = "index.php" class="back">
             Retour à la liste des billets
         </a>
         <?php
@@ -18,15 +18,15 @@
         </h1>
         <?php
             while ($data = $comment->fetch()) {
-                echo '<p><strong>' . htmlspecialchars($data['auteur']) . '</strong> le ' . $data['date_day_post'] . '</p> 
-                <div class="col-lg-3 col-lg-offset-3"><p> ' . nl2br(htmlspecialchars($data['commentaire'])) . '</p></div>';
+                echo '<div class ="row"><div class="col-lg-3"><p class="titreComment"><strong>' . htmlspecialchars($data['auteur']) . '</strong> le ' . $data['date_day_post'] . '</p> 
+                <p class="comment"> ' . nl2br(htmlspecialchars($data['commentaire'])) . '<a href="index.php?action=alert"> Signalez </a></p></div></div>';
             }
         ?>
         <form method="post" action="index.php?action=addComment&amp;id=<?= $_GET['id'] ?>">
             Votre commentaire: <br/>
-            <textarea name="message"></textarea><br/>
+            <textarea rows="5" cols="64" name="message"></textarea><br/>
             <input type="hidden" name="id_post" value= <?php echo $_GET['id'] ?> />
-            <input type="submit" value="Envoyez"/>
+            <input class="send" type="submit" value="Envoyez"/>
         </form>
         <?php $comment->closeCursor(); ?>
 <?php $content = ob_get_clean(); ?>
